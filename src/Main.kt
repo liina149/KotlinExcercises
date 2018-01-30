5d6af65f9a9d37eb40de0ef9cf5daad7b36b2ada
Liina Tuttar <liina.tuttar@gmail.com>
2018-01-31 00:54:12 +0200

    Initial commit

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,13 @@ fun main(args : Array<String>) {
 
     var nimi = "Liina"
     println("Hello $nimi")
+
+    var i = 23
+    var j = 36
+
+
+
+    fun liida(i: Int, j: Int): Int = i+j
+
+    print("$i + $j ${ liida(i,j) }")
 }
